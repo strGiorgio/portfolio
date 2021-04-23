104dcd21310e9007fabeb979e35441db4ba63264
Giorgio Alefe <giorgioa2002@gmail.com>
2021-04-23 15:04:51 -0300

    Finished the main-projects scripts

--- scripts/components/main-projects-script.js
+++ scripts/components/main-projects-script.js
@@ -1,4 +1,3 @@
-var backG = window.document.querySelector('.main-section._projects')
 var n = 0
 
 function lClick() {
@@ -24,16 +23,53 @@ function rClick() {
 }
 
 function changeBg() {
+    var background = document.querySelector('.main-section._projects')
+    var boxBg = document.querySelector('.box-projects')
+    var title = document.querySelector('.second-title._projects')
+    var imgBg = document.querySelector('.wrapper-img')
+    var img = document.querySelector('.imagem-projects')
+    var text = document.querySelector('.box-projects .main-text')
+
     if (n == 0) {
-        backG.style.background = '#7D5CF2'
-        console.log('roxin')
+        //Python Projects
+        //background
+        background.style.background= '#0d0d0d'
+        boxBg.style.background= '#717777'
+        //image
+        imgBg.style.background= '#363436'
+        img.setAttribute('src', 'link')
+        //title and text
+        title.innerText = 'PyMob'
+        title.style.color = '#000'
+        text.innerText = 'Texto Pymob'
+        text.style.color = '#fff'
     } 
     else if (n == 1) {
-        backG.style.background = 'green'
-        console.log('verdin')
+        //GitHub Projects
+        //background
+        background.style.background= '#13273a'
+        boxBg.style.background= '#64809b'
+        //image
+        imgBg.style.background= '#13273a'
+        img.setAttribute('src', 'link')
+        //title and text
+        title.innerText = 'Github Projects'
+        title.style.color = '#000'
+        text.innerText = 'Texto github projects'
+        text.style.color = '#fff'
     } 
     else {
-        backG.style.background = 'blue'
-        console.log('zulzin')
+        //Projects
+        //background
+        background.style.background= '#b8b8b8'
+        boxBg.style.background= 'aliceblue'
+        //image
+        imgBg.style.background= '#b8b8b8'
+        img.setAttribute('src', 'link')
+        //title and text
+        title.innerText = 'Others Projects'
+        title.style.color = '#000'
+        text.innerText = 'Texto others projects'
+        text.style.color = '#000'
     }
 }
